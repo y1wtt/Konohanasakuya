601804d8e9f4410d90e5ec702b21134f699eb2b5
awataki <awataki1206@gmail.com>
2022-06-29 06:07:50 +0900

    add config.dev.js

---
+++ vite.config.dev.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  root:path.resolve(__dirname, './'),
+  build: {
+    // lib: {
+    //   entry: path.resolve(__dirname, 'src/main.ts'),
+    //   name: 'Konohanasakuya',
+    //   fileName: (format) => `konohanasakuya.${format}.js`
+    // },
+    rollupOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: ['vue'],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css')
+            return 'konohanasakuya.css';
+          return assetInfo.name;
+        },
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       vue: 'Vue'
+    //     }
+      }
+    }
+  }
+})
