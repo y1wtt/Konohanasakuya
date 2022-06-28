5a17d4071f3a4c3ae48e0c0bd5497fbae22aa4b6
awataki <awataki1206@gmail.com>
2022-06-29 04:49:57 +0900

    impl textFieldEvent

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
-import './main.scss';
-console.log('loaded')
+import './main.scss'
+
+const textField: NodeListOf<HTMLInputElement> = document.querySelectorAll<HTMLInputElement>('.kh-text-field>input')
+textField.forEach((tf) => {
+  tf.addEventListener('focus', (_: Event) => {
+    tf.labels?.forEach((l) => {
+      l.parentElement?.classList.add('--active')
+    })
+  })
+  tf.addEventListener('blur', (_: Event) => {
+    tf.labels?.forEach((l) => {
+      l.parentElement?.classList.remove('--active')
+    })
+  })
+})
